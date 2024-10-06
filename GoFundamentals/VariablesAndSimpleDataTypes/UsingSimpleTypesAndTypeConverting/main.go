759dac8574650fb88657a0da70990a2ef6700b16
altair961 <altair89@list.ru>
2024-10-06 19:54:02 +0300

    Bassic type conversion examples were added.

--- GoFundamentals/VariablesAndSimpleDataTypes/UsingSimpleTypesAndTypeConverting/main.go
+++ GoFundamentals/VariablesAndSimpleDataTypes/UsingSimpleTypesAndTypeConverting/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Gophers!")
+	var a string
+	a = "foo"
+
+	fmt.Println(a)
+
+	var b int = 99
+	fmt.Println(b)
+
+	var c = true
+	fmt.Println(c)
+
+	d := 3.1415 //float 64
+	fmt.Println(d)
+
+	var e int = int(d)
+	fmt.Println(e)
+
+	// var f int8 = 54
+	// var g int16 = f
+	// fmt.Println(e)
 }
