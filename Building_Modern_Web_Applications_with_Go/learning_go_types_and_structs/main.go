864762bee449e61258a66482f36f85a1180d18cb
altair961 <altair89@list.ru>
2024-10-09 12:30:12 +0300

    A larger scoped variable was shadowed by a smaller scoped variable.

---
+++ Building_Modern_Web_Applications_with_Go/learning_go_types_and_structs/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "log"
+
+var s = "seven"
+
+func main() {
+	var s2 = "six"
+
+	log.Println("s is", s)
+	log.Println("s2 is", s2)
+
+	saySomething("xxx")
+}
+
+func saySomething(s string) (string, string) {
+	log.Println("s from the saySomething func is", s)
+	return s, "World"
+}
