e59bfc8a8bc47e206b421da4e995cf8aec1a4803
altair961 <altair89@list.ru>
2024-10-10 13:28:03 +0300

    A prepopulated slice was added.

--- Building_Modern_Web_Applications_with_Go/learning_go_slices/main.go
+++ Building_Modern_Web_Applications_with_Go/learning_go_slices/main.go
@@ -1,18 +1,8 @@
 package main
 
-import (
-	"log"
-	"sort"
-)
+import "log"
 
 func main() {
-	var mySlice []int
-
-	mySlice = append(mySlice, 2)
-	mySlice = append(mySlice, 1)
-	mySlice = append(mySlice, 3)
-
-	sort.Ints(mySlice)
-
-	log.Println(mySlice)
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(numbers)
 }
