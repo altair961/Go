cd519a41a13cc91504686c35958e84e4dc456b4f
altair961 <altair89@list.ru>
2024-10-10 13:14:56 +0300

    A basic slice example was added.

---
+++ Building_Modern_Web_Applications_with_Go/learning_go_slices/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "log"
+
+func main() {
+	var mySlice []string
+
+	mySlice = append(mySlice, "Trevor")
+	mySlice = append(mySlice, "John")
+	mySlice = append(mySlice, "Mary")
+
+	log.Println(mySlice)
+}
