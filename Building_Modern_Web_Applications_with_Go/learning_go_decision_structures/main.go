2d3e6fd01082f67c391509a4aa7e8ce5b346877b
altair961 <altair89@list.ru>
2024-10-10 13:37:43 +0300

    Basic if else statement was added.

---
+++ Building_Modern_Web_Applications_with_Go/learning_go_decision_structures/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "log"
+
+func main() {
+	var isTrue bool
+	isTrue = false
+
+	if isTrue {
+		log.Println("isTrue is", isTrue)
+	} else {
+		log.Println("isTrue is", isTrue)
+	}
+}
